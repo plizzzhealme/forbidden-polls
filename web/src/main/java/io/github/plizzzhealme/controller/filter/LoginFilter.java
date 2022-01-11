d7606fed6428a8fa01602a6913f472964d624c62
plizzzhealme <plizzz.healme@gmail.com>
2022-01-11 14:12:52 +0300

    complete with LoginFilter (version 1.0) lol

--- web/src/main/java/io/github/plizzzhealme/controller/filter/LoginFilter.java
+++ web/src/main/java/io/github/plizzzhealme/controller/filter/LoginFilter.java
@@ -2,8 +2,6 @@ package io.github.plizzzhealme.controller.filter;
 
 import io.github.plizzzhealme.controller.command.CommandProvider;
 import io.github.plizzzhealme.controller.util.ControllerUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -15,44 +13,22 @@ import java.io.IOException;
 @WebFilter("/*")
 public class LoginFilter implements Filter {
 
-    private static final Logger logger = LogManager.getLogger(LoginFilter.class);
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
-
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
 
         String command = servletRequest.getParameter(CommandProvider.COMMAND);
 
-        if (CommandProvider.TO_USER_PAGE_COMMAND.equals(command)) {
-
-            logger.info("attempt to get access to user page");
-
+        if (CommandProvider.TO_USER_PAGE_COMMAND.equals(command)) { // if command = to_user_page
             HttpSession session = ((HttpServletRequest) servletRequest).getSession();
 
-            if (session.getAttribute(ControllerUtil.USER) == null) {
-                logger.error("user is not logged in, redirect to login page");
-
+            if (session.getAttribute(ControllerUtil.USER) == null) { // if logged in
                 ((HttpServletResponse) servletResponse).sendRedirect(ControllerUtil.TO_AUTHORIZATION_PAGE_REDIRECT);
-            } else {
-                logger.info("user is logged in");
-
+            } else { // if not logged in
                 filterChain.doFilter(servletRequest, servletResponse);
             }
-        } else {
-            logger.info("Command = {} is not required to be logged in", command);
-
+        } else { // if other commands
             filterChain.doFilter(servletRequest, servletResponse);
         }
-
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
     }
 }
