7b55ba517c37829732b7bfd823fee00e482f5875
plizzzhealme <plizzz.healme@gmail.com>
2022-01-11 13:33:35 +0300

    add logger to filter

--- web/src/main/java/io/github/plizzzhealme/controller/filter/LoginFilter.java
+++ web/src/main/java/io/github/plizzzhealme/controller/filter/LoginFilter.java
@@ -2,14 +2,20 @@ package io.github.plizzzhealme.controller.filter;
 
 import io.github.plizzzhealme.controller.command.CommandProvider;
 import io.github.plizzzhealme.controller.util.ControllerUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter("/*")
 public class LoginFilter implements Filter {
+
+    private static final Logger logger = LogManager.getLogger(LoginFilter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -19,17 +25,27 @@ public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
-        System.out.println("HELLO!");
 
-        if (CommandProvider.TO_USER_PAGE_COMMAND.equals(servletRequest.getParameter(CommandProvider.COMMAND))) {
-            if (servletRequest.getAttribute(ControllerUtil.USER) == null) {
-                System.out.println("IF!");
+        String command = servletRequest.getParameter(CommandProvider.COMMAND);
+
+        if (CommandProvider.TO_USER_PAGE_COMMAND.equals(command)) {
+
+            logger.info("attempt to get access to user page");
+
+            HttpSession session = ((HttpServletRequest) servletRequest).getSession();
+
+            if (session.getAttribute(ControllerUtil.USER) == null) {
+                logger.error("user is not logged in, redirect to login page");
+
                 ((HttpServletResponse) servletResponse).sendRedirect(ControllerUtil.TO_AUTHORIZATION_PAGE_REDIRECT);
             } else {
-                System.out.println("ELSE!");
+                logger.info("user is logged in");
+
                 filterChain.doFilter(servletRequest, servletResponse);
             }
         } else {
+            logger.info("Command = {} is not required to be logged in", command);
+
             filterChain.doFilter(servletRequest, servletResponse);
         }
 
