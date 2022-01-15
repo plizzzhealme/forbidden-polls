53481b93c60619659eb1fe3ebd49a92baedd3bbf
plizzzhealme <plizzz.healme@gmail.com>
2022-01-15 21:30:07 +0300

    add authorization filter for to survey page command

--- web/src/main/java/io/github/plizzzhealme/controller/filter/AuthorizationFilter.java
+++ web/src/main/java/io/github/plizzzhealme/controller/filter/AuthorizationFilter.java
@@ -9,9 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @WebFilter("/controller")
 public class AuthorizationFilter implements Filter {
+
+    private final Set<String> commandsRequiringAuthorization = new HashSet<>();
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        commandsRequiringAuthorization.add(CommandProvider.TO_USER_PAGE_COMMAND);
+        commandsRequiringAuthorization.add(CommandProvider.TO_SURVEY_PAGE_COMMAND);
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
@@ -19,7 +29,7 @@ public class AuthorizationFilter implements Filter {
 
         String command = servletRequest.getParameter(CommandProvider.COMMAND);
 
-        if (CommandProvider.TO_USER_PAGE_COMMAND.equals(command)) { // if command = to_user_page
+        if (commandsRequiringAuthorization.contains(command)) { // if command = to_user_page
             HttpSession session = ((HttpServletRequest) servletRequest).getSession();
 
             if (session.getAttribute(ControllerUtil.USER_ID) == null) { // if logged in
