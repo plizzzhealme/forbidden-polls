c5c3c0f965802d799c4885953c61228e6d0dc3d6
plizzzhealme <plizzz.healme@gmail.com>
2022-01-27 16:03:45 +0300

    remove init from CharsetFilter

--- web/src/main/java/io/github/plizzzhealme/controller/filter/CharsetFilter.java
+++ web/src/main/java/io/github/plizzzhealme/controller/filter/CharsetFilter.java
@@ -6,25 +6,13 @@ import java.io.IOException;
 public class CharsetFilter implements Filter {
 
     public static final String UTF_8 = "utf-8";
-    public static final String REQUEST_ENCODING = "requestEncoding";
-
-    private String encoding;
-
-    @Override
-    public void init(FilterConfig filterConfig) {
-        encoding = filterConfig.getInitParameter(REQUEST_ENCODING);
-
-        if (encoding == null) {
-            encoding = UTF_8;
-        }
-    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
 
-        servletRequest.setCharacterEncoding(encoding);
-        servletResponse.setCharacterEncoding(encoding);
+        servletRequest.setCharacterEncoding(UTF_8);
+        servletResponse.setCharacterEncoding(UTF_8);
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
